b7974dc6e2f798e53343cd660a018a0355db3813
AdiletExp <turakulov_adilet@mail.ru>
2022-10-27 21:43:48 +0600

    create new activity from Aziz

--- app/src/main/java/com/example/lesson8/MainActivity4.java
+++ app/src/main/java/com/example/lesson8/MainActivity4.java
@@ -1,14 +1,21 @@
 package com.example.lesson8;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity4 extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main4);
+    protected void onCreate(Bundle sState) {
+        super.onCreate(sState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    @Override
+    public boolean onCreatePanelMenu(int featureId, @NonNull Menu menu) {
+        return super.onCreatePanelMenu(featureId, menu);
     }
 }
