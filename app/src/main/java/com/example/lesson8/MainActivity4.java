97e7cbae9ac4121769f9655af3d280751c326697
AdiletExp <turakulov_adilet@mail.ru>
2022-10-27 21:42:08 +0600

    create new activity from Aziz

--- app/src/main/java/com/example/lesson8/MainActivity4.java
+++ app/src/main/java/com/example/lesson8/MainActivity4.java
@@ -7,8 +7,7 @@ import android.os.Bundle;
 public class MainActivity4 extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main4);
+    public void setFinishOnTouchOutside(boolean finish) {
+        super.setFinishOnTouchOutside(finish);
     }
 }
